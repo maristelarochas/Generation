8e82874676f14f5297e3a58c41bda143a3d2d6a4
Maristela Rocha <maristela.rocha.silva@gmail.com>
2025-08-26 21:11:13 -0300

    Fix exercise 2

--- Exercicios/EntradaSaidaOperadores/Exercicio_02.js
+++ Exercicios/EntradaSaidaOperadores/Exercicio_02.js
@@ -1,12 +1,12 @@
 const leia = require('readline-sync');
 
-let nota1, nota2, nota3, nota4, media
+let nota1, nota2, nota3, nota4, media, entrada
 
 nota1 = leia.questionInt("Digite a primeira nota: ");
 nota2 = leia.questionInt("Digite a segunda nota: ");
 nota3 = leia.questionInt("Digite a terceira nota: ");
 nota4 = leia.questionInt("Digite a quarta nota: ");
 
-mediaFinal = (nota1 + nota2 + nota3 + nota4)/4;
+media = (nota1 + nota2 + nota3 + nota4)/4;
 
-console.log("Media final: ", mediaFinal);
+console.log("Media final: ", media);
